8f56e536c0c4e7abc52dd10f231c6948da8713b7
Huangxiaoqin <13672167939@163.com>
2017-08-21 21:17:10 +0800

    advertisement page

---
+++ javascript/base.js
@@ -0,0 +1,18 @@
+'use strict';
+window.onload = function ()
+{
+	var oLi = document.getElementById("tab").getElementsByTagName("li");
+	var oUl = document.getElementById("display_content").getElementsByTagName("ul");
+	
+	for(var i = 0; i < oLi.length; i++)
+	{
+		oLi[i].index = i;
+		oLi[i].onmouseover = function ()
+		{
+			for(var n = 0; n < oLi.length; n++) oLi[n].className="";
+			this.className = "current";
+			for(var n = 0; n < oUl.length; n++) oUl[n].style.display = "none";
+			oUl[this.index].style.display = "block"
+		}	
+	}
+}
